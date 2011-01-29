0637fb6d768baa8f60291a33ca2041bdcf4e5437
Bob <bertboy711@gmail.com>
2011-01-29 18:16:22 -0500

    Added the communication classes

---
+++ mcsharpbot.communication/Debug.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mcsharpbot.communication
+{
+    public class Debug
+    {
+
+        public static void Info(string msg)
+        {
+            Console.WriteLine("INFO: " + msg);
+        }
+
+        public static void Info(Exception e)
+        {
+            Console.WriteLine("INFO: " + e);
+        }
+
+        public static void Warning(string msg)
+        {
+            Console.WriteLine("INFO: " + msg);
+        }
+
+        public static void Warning(Exception e)
+        {
+            Console.WriteLine("INFO: " + e);
+        }
+
+        public static void Severe(string msg)
+        {
+            Console.WriteLine("INFO: " + msg);
+        }
+
+        public static void Severe(Exception e)
+        {
+            Console.WriteLine("INFO: " + e);
+        }
+
+    }
+}
---
+++ mcsharpbot.communication/MinecraftServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace mcsharpbot.communication
+{
+    public class MinecraftServer
+    {
+
+        private Socket Socket;
+
+        public IPEndPoint ServerAddress;
+        public string ServerName, ServerMOTD, Password, Hash;
+        public long MapSeed, Time;
+
+        public MinecraftServer(Socket socket)
+        {
+            this.Socket = socket;
+            this.ServerAddress = (IPEndPoint)this.Socket.RemoteEndPoint;
+            this.ServerName = this.ServerMOTD = this.Password = this.Hash = "";
+            this.MapSeed = this.Time = 0L;
+        }
+    }
+}
