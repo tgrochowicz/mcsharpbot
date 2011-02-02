5b64c57f7500bcc05a9e2451e65b409ad289ea3d
Bob <bertboy711@gmail.com>
2011-02-01 21:55:41 -0500

    Fixed some bugs

---
+++ mcsharpbot.bots/MoveBot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mcsharpbot.bots
+{
+    public class MoveBot : MCBotBase
+    {
+        public override void BeginAction()
+        {
+            Connection.ChatMessageReceived += new communication.MCServerConnection.MinecraftClientChatEventHandler(Connection_ChatMessageReceived);
+            base.BeginAction();
+        }
+
+        void Connection_ChatMessageReceived(object sender, communication.MinecraftClientChatEventArgs args)
+        {
+            if (!args.User.Equals(Connection.Username))
+            {
+                if (args.Message.Trim() == "move")
+                {
+                    Connection.PlayerRotation.Yaw = 150;
+                    Connection.SetPlayerLocation(Connection.PlayerLocation);
+                }
+            }
+        }
+
+        public override void PauseAction()
+        {
+            base.PauseAction();
+        }
+
+        public override void StopAction()
+        {
+            base.StopAction();
+        }
+
+
+        public override string BotName()
+        {
+            return "MoveBot";
+        }
+
+
+    }
+}
